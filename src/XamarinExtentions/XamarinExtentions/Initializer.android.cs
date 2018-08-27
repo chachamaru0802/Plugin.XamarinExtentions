641348c6817115e3f938311149f52d5abd582884
chachamaru0802 <chachamaru.0802.k@gmail.com>
2018-08-27 22:22:58 +0900

    Initializerを追加

---
+++ src/XamarinExtentions/XamarinExtentions/Initializer.android.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.XamarinExtentions
+{
+  public static  class Initializer
+    {
+        public static void Init()
+        {
+
+        }
+    }
+}
