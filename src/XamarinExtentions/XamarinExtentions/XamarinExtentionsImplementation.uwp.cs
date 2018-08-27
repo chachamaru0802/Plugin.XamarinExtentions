c0fd34aeb37db5851b6d4444a96c8b9a257974f1
chachamaru0802 <chachamaru.0802.k@gmail.com>
2018-08-27 21:35:55 +0900

    プロジェクト新規作成

---
+++ src/XamarinExtentions/XamarinExtentions/XamarinExtentionsImplementation.uwp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.XamarinExtentions
+{
+    /// <summary>
+    /// Interface for $safeprojectgroupname$
+    /// </summary>
+    public class XamarinExtentionsImplementation : IXamarinExtentions
+    {
+    }
+}
